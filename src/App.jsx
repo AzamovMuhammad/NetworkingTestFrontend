ff6fe347ba3fe6b1e37b4898e9b02f5315cf9a15
AzamovMuhammad <azamov.dragon@gmail.com>
2025-05-29 17:10:49 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [name, setName] = useState("");
+
+  // GET
+  const fetchUsers = async () => {
+    const res = await fetch("https://networkingtestbackend.onrender.com/api/users");
+    const data = await res.json();
+    setUsers(data);
+  };
+
+  // POST
+  const addUser = async () => {
+    await fetch("https://networkingtestbackend.onrender.com/api/users", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name }),
+    });
+    setName("");
+    fetchUsers();
+  };
+
+  // DELETE
+  const deleteUser = async (id) => {
+    await fetch(`https://networkingtestbackend.onrender.com/api/users/${id}`, { method: "DELETE" });
+    fetchUsers();
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Users List</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addUser();
+        }}>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter name"
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} <button onClick={() => deleteUser(user.id)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
